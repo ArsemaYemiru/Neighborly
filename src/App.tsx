879a4a10412edc53aa9f80734bc72f2d656d35c7
Arsema Yemiru <arsemayemiru610@gmail.com>
2025-08-06 10:27:57 +0300

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import { Flex, Button } from '@mantine/core'
+
+function App() {
+
+  return (
+    <>
+      <Flex>
+        <Button>
+          hello world
+        </Button>
+      </Flex>
+    </>
+  )
+}
+
+export default App
