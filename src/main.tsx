01c79f8a15c51765b3890d05211a7bfbb9041000
Leul Yohannes <106162490+ltyohannes955@users.noreply.github.com>
2025-08-06 10:53:40 +0300

    done setting up capaciter for the project

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,10 @@
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import { createRoot } from "react-dom/client";
+import App from "./App.tsx";
 import "@mantine/core/styles.css";
 import { MantineProvider } from "@mantine/core";
 
-createRoot(document.getElementById('root')!).render(
+createRoot(document.getElementById("root")!).render(
   <MantineProvider>
     <App />
-  </MantineProvider>,
-)
+  </MantineProvider>
+);
